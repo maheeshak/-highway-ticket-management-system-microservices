b7d5542c21b4ca482d3ecc2777a8686e05610acc
Maheesha <maheeshafernando18@gmail.com>
2024-07-04 16:44:50 +0530

    implement service reg

---
+++ ticket-service-registry/src/test/java/lk/ijse/ticket_service_registry/TicketServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.ticket_service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
