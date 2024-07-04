09e2fc29533c2c8191390ae4899be486495ae0ac
Maheesha <maheeshafernando18@gmail.com>
2024-07-04 16:45:48 +0530

    implement config server

---
+++ ticket-config-server/src/test/java/lk/ijse/ticket_config_server/TicketConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.ticket_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
